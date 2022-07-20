9aee56e2f604373928c79cdc79f35178f0879baf
Rajesh-code-droid <nandirajesh6@gmail.com>
2022-07-20 15:45:28 +0530

    bump button and timer

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,29 @@
-import { useContext } from "react";
+import { useContext , useEffect, useState} from "react";
 import CartIcon from "../Cart/CartIcon";
 import Classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
 const HeaderCartButton = (props) => {
+  const[btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
+  const {items}= cartCtx;
+  const buttonClass = `${Classes.button} ${btnIsHighlighted ? Classes.bump : ''}`;
+  useEffect(()=>{
+    if(cartCtx.items.length === 0){
+      return;
+    }
+    setBtnIsHighlighted(true);
+    const timer= setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+    return ()=>{
+      clearTimeout(timer);
+    };
+  },[items]);
   return (
-    <button className={Classes.button} onClick={props.onClick}>
+    <button className={buttonClass} onClick={props.onClick}>
       <span className={Classes.icon}>
         <CartIcon />
       </span>
